857ea7cd1728ed73f1ecde403ac4a5bb196a48af
audu0132ar <audumbarmore23@gmail.com>
2026-08-26 14:30:00 +0530

    feat(catalog): add Bangle size conversion matrix from Indian anna to mm diameter

---
+++ backend/data/specs/bangleSizes.js
@@ -0,0 +1,6 @@
+export const BANGLE_SIZE_CHART = [
+  { size: "2-4", diameterMm: 57.2, circumferenceMm: 179.6 },
+  { size: "2-6", diameterMm: 60.3, circumferenceMm: 189.5 },
+  { size: "2-8", diameterMm: 63.5, circumferenceMm: 199.5 },
+  { size: "2-10", diameterMm: 66.7, circumferenceMm: 209.4 },
+];
