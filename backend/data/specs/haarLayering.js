ef73d3eaa2cf4af2e8f7c862768c3d87958be546
audu0132ar <audumbarmore23@gmail.com>
2026-08-26 11:00:00 +0530

    feat(catalog): add Rani Haar layering guide with drop length intervals

---
+++ backend/data/specs/haarLayering.js
@@ -0,0 +1,6 @@
+export const HAAR_LAYERING_GUIDE = {
+  tier1_Choker: "12-14 inches (snug collar fit)",
+  tier2_PrincessNecklace: "17-19 inches (collarbone rest)",
+  tier3_MatineePiece: "20-24 inches (chest center)",
+  tier4_RaniHaar: "28-34 inches (below bust / navel highlight)",
+};
