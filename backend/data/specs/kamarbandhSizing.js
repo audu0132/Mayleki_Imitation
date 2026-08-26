cc65a64ded0003d1ec6555c8ba83ea42882aeff9
audu0132ar <audumbarmore23@gmail.com>
2026-08-26 12:30:00 +0530

    feat(catalog): add Kamarbandh waist belt waistline circumference sizing matrix

---
+++ backend/data/specs/kamarbandhSizing.js
@@ -0,0 +1,6 @@
+export const KAMARBANDH_SIZING = [
+  { size: "S", waistInches: "26 - 30", chainLinks: 18 },
+  { size: "M", waistInches: "30 - 34", chainLinks: 22 },
+  { size: "L", waistInches: "34 - 38", chainLinks: 26 },
+  { size: "XL", waistInches: "38 - 44", chainLinks: 30 },
+];
