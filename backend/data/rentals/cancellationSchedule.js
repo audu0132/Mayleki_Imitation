45318567cf806fa3153f1760e34e5a2f8da6e480
audu0132ar <audumbarmore23@gmail.com>
2026-08-27 14:30:00 +0530

    feat(rentals): define tiered cancellation window refund schedule

---
+++ backend/data/rentals/cancellationSchedule.js
@@ -0,0 +1,6 @@
+export const CANCELLATION_SCHEDULE = [
+  { minDaysNotice: 14, refundPercent: 100, deductionPercent: 0 },
+  { minDaysNotice: 7, refundPercent: 85, deductionPercent: 15 },
+  { minDaysNotice: 3, refundPercent: 50, deductionPercent: 50 },
+  { minDaysNotice: 0, refundPercent: 0, deductionPercent: 100 },
+];
