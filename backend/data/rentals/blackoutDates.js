9fa584f393b3ca49cd24c251dc02a7cdccafb041
audu0132ar <audumbarmore23@gmail.com>
2026-08-27 16:30:00 +0530

    feat(rentals): configure Diwali and auspicious wedding dates blackout calendar

---
+++ backend/data/rentals/blackoutDates.js
@@ -0,0 +1,5 @@
+export const AUSPICIOUS_DATES_2026 = [
+  "2026-10-20", "2026-10-21", "2026-10-22", // Diwali peak
+  "2026-11-15", "2026-11-22", "2026-11-28", // Dev Uthani Ekadashi wedding muhurats
+  "2026-12-04", "2026-12-11", "2026-12-18", // December bridal rush
+];
