fec90440348b18ef31c671c40148cf5001cbc2f4
audu0132ar <audumbarmore23@gmail.com>
2026-08-27 16:00:00 +0530

    feat(rentals): add ceremonial jewellery replacement valuation guidelines

---
+++ backend/data/rentals/valuationGuidelines.js
@@ -0,0 +1,5 @@
+export const VALUATION_GUIDELINES = {
+  replacementMultiplier: 4.5, // Replacement retail is ~4.5x total 3-day rental price
+  repairableStoneDeduction: 200,
+  brokenDoriReplacement: 150,
+};
