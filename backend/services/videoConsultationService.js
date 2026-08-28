71f1c53f2e24e72fc110d54d79dd4d9e4c947379
audu0132ar <audumbarmore23@gmail.com>
2026-08-28 10:00:00 +0530

    feat(concierge): implement video consultation appointment slot scheduler

---
+++ backend/services/videoConsultationService.js
@@ -0,0 +1,8 @@
+export const generateAppointmentSlots = (dateStr) => {
+  return [
+    { time: "11:00 AM", available: true },
+    { time: "02:30 PM", available: true },
+    { time: "05:00 PM", available: true },
+    { time: "07:30 PM", available: true },
+  ];
+};
