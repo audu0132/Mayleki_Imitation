50b6546f34f1649ddf510e38b7b9b88850d3189e
audu0132ar <audumbarmore23@gmail.com>
2026-08-29 11:30:00 +0530

    style(tokens): configure micro-interaction spring animation constants

---
+++ frontend/src/styles/tokens/animations.js
@@ -0,0 +1,5 @@
+export const SPRING_TRANSITIONS = {
+  gentle: { type: "spring", stiffness: 120, damping: 14 },
+  snappy: { type: "spring", stiffness: 300, damping: 25 },
+  fadeDuration: 0.35,
+};
