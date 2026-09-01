a4a02056343ad452f6d314d1db898d2999121b0c
audu0132ar <audumbarmore23@gmail.com>
2026-09-01 13:30:00 +0530

    feat(seo): add multi-language hreflang tags for regional customer accessibility

---
+++ frontend/src/utils/hreflangTags.js
@@ -0,0 +1,5 @@
+export const HREFLANG_CONFIG = [
+  { lang: "en-IN", href: "https://mayleki.com/" },
+  { lang: "mr-IN", href: "https://mayleki.com/mr/" },
+  { lang: "x-default", href: "https://mayleki.com/" },
+];
