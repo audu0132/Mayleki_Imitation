1da6018846a20e3ae51935925d668347d9c8c578
audu0132ar <audumbarmore23@gmail.com>
2026-09-16 08:42:00 +0530

    feat(seo): add Organization and LocalBusiness schema for Mayleki Pune boutique

---
+++ frontend/src/utils/businessSchema.js
@@ -0,0 +1,16 @@
+export const MAYLEKI_BUSINESS_SCHEMA = {
+  "@context": "https://schema.org",
+  "@type": "JewelryStore",
+  name: "Mayleki Imitation Jewellery",
+  image: "https://images.unsplash.com/photo-1601121141461-9d6647bca1ed",
+  address: {
+    "@type": "PostalAddress",
+    streetAddress: "FC Road, Deccan Gymkhana",
+    addressLocality: "Pune",
+    addressRegion: "Maharashtra",
+    postalCode: "411005",
+    addressCountry: "IN",
+  },
+  telephone: "+91-9876543210",
+  priceRange: "₹₹",
+};
