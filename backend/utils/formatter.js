f4d943fc7d0b607e2e16ea2058c1578c2a8c7736
audu0132ar <audumbarmore23@gmail.com>
2026-09-17 16:00:00 +0530

    feat(backend): add response formatter utility

---
+++ backend/utils/formatter.js
@@ -0,0 +1,4 @@
+﻿// Response formatter
+const success = (data, msg = 'OK') => ({ success: true, message: msg, data });
+const error = (msg = 'Error') => ({ success: false, message: msg });
+module.exports = { success, error };
