8d04dd42cb1ffd4b7fbb813dcb75f2356d0441c4
audu0132ar <audumbarmore23@gmail.com>
2026-09-18 14:50:00 +0530

    feat(backend): add crypto utility helpers

---
+++ backend/utils/crypto.js
@@ -0,0 +1,5 @@
+﻿// Crypto utility
+const crypto = require('crypto');
+const hash = (s) => crypto.createHash('sha256').update(s).digest('hex');
+const randomToken = (n = 32) => crypto.randomBytes(n).toString('hex');
+module.exports = { hash, randomToken };
