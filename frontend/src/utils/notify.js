4d0160f6e120fbba83a1e99eab6148dc8442c00e
audu0132ar <audumbarmore23@gmail.com>
2026-09-19 12:00:00 +0530

    feat(frontend): add notification helper utilities

---
+++ frontend/src/utils/notify.js
@@ -0,0 +1,4 @@
+﻿// Notification helper
+export const notify = (msg, type = 'info') => { console.log('[' + type.toUpperCase() + '] ' + msg); };
+export const notifySuccess = (msg) => notify(msg, 'success');
+export const notifyError = (msg) => notify(msg, 'error');
